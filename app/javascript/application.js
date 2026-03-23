bc81fe14b52e44c52da5f16bb2711ccc74486e03
Jared <jared@evilpandas>
2026-03-23 16:29:05 -0600

    initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import "flowbite/dist/flowbite.turbo.js";
+
+import "flowbite-datepicker"
+import "flowbite-datepicker/dist/flowbite-datepicker.turbo.js";
